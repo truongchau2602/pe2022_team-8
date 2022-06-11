787839ed6eef3aa12ba9e1ef1b77fa2c22081cc6
Chau Truong <16076@student.vgu.edu.vn>
2022-06-11 23:54:23 +0700

    Adding aboutus, contact, login page and configure other file to work in node

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ app.listen(4000, ()=>{
 app.get("/",(req,res)=>{
     res.render("home")
 })
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+})
 
 app.get("/auth/login", (req, res)=>{
+    res.render("login")
+})
+app.get("/about", (req,res)=>{
+    res.render("about")
+})
+app.get("/login",(req,res)=>{
     res.render("login")
 })
